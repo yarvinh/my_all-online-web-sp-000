0b2b7400f52884698d43b05a7df79a46d65fe7a5
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-26 02:01:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
+  if block_given?
+    counter = 0
+    array = []
+    while counter < collection.length
+    array <<  yield(collection[counter])
+      counter += 1
+    end
+    array
+  end
 
 end
