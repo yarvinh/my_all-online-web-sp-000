2e3e167571edd71003ae99bc5d2b913d8b37b683
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-26 02:13:08 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
-  if block_given?
-    counter = 0
-    array = []
-    while counter < collection.length
-    array <<  yield(collection[counter])
-      counter += 1
-    end
-    array
-  end
-
-end
+       if block_given?
+         counter = 0
+         array = []
+         while counter < collection.length
+         array <<  yield(collection[counter])
+           counter += 1
+         end
+         if array.include?(false)
+          false
+         else 
+           true
+         end
+       else
+          puts  "No array was pass!"
+       end
+     
+     end
